eaf0f4d02fc1a2fcf982a0774485ae98c0ccbc7f
Drew Payton <drewpayton18@gmail.com>
2022-05-03 14:48:12 -0500

    adding some save features

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 var rows = document.getElementsByClassName("custom-row");
 var currentHour = parseInt(moment().format('H'));
+console.log(currentHour)
+
+saveData();
+
+
+
+
+
+
+function saveData() {
+
+var nine = localStorage.getItem("nineTime");
+var ten = localStorage.getItem("tenTime");
+var eleven = localStorage.getItem("elevenTime");
+var twelve = localStorage.getItem("twelveTime");
+var thirteen = localStorage.getItem("thirteenTime");
+var fourteen = localStorage.getItem("fourteenTime");
+var fifteen = localStorage.getItem("fifteenTime");
+var sixteen = localStorage.getItem("sixteenTime");
+var seventeen = localStorage.getItem("seventeenTime");
+
+if (nine === null) {
+  console.log("this is null")
+}else {
+  console.log("this is active")
+}
+
+};
+
+
 
 Array.from(rows).forEach(row => {
   var boxId = row.id;
@@ -9,21 +39,27 @@ Array.from(rows).forEach(row => {
     boxHour = parseInt(boxId);
   }
   if (boxHour) {
-    // Compares row id to current hour and sets color accordingly
     if (currentHour === boxHour) {
-      setColor(row, "red");
+      setColor(row, "#F08080");
+      setBorder(row, "groove");
     } else if ((currentHour < boxHour)) {
       setColor(row, "#8A9A5B");
+      setBorder(row, "groove");
     } else if ((currentHour > boxHour)) {
       setColor(row, "lightgrey");
+      setBorder(row, "groove");
     } else {
       setColor(row, "white");
+      setBorder(row, "groove");
     }
   }
 });
 
 function setColor(element, color) {
   element.style.backgroundColor = color;
+}
+function setBorder(element, border) {
+  element.style.border = border;
 }
 
 
